ce24bd989ddb178696d17b053fdda438e0ec082d
Garfieldcmix <chayanon.nin@gmail.com>
2025-06-25 00:42:07 +0700

    Add todo-comment plugin

--- lua/plugins/appearances.lua
+++ lua/plugins/appearances.lua
@@ -42,4 +42,11 @@ return {
             })
         end,
     },
+    {
+        -- Highlight todo, notes, etc in comments
+        "folke/todo-comments.nvim",
+        event = "VimEnter",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        opts = { signs = false },
+    },
 }
